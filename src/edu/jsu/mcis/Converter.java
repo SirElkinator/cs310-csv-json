2b9d9d0fb70f25745e54c7ebbf02b87293e6d7cc
Chase Sparks <Chase Sparks@LAPTOP-IOUHCIB8>
2018-09-30 15:41:39 -0500

    Completed Project2

--- src/edu/jsu/mcis/Converter.java
+++ src/edu/jsu/mcis/Converter.java
@@ -8,58 +8,15 @@ import org.json.simple.parser.*;
 
 public class Converter {
     
-    /*
-    
-        Consider the following CSV data:
-        
-        "ID","Total","Assignment 1","Assignment 2","Exam 1"
-        "111278","611","146","128","337"
-        "111352","867","227","228","412"
-        "111373","461","96","90","275"
-        "111305","835","220","217","398"
-        "111399","898","226","229","443"
-        "111160","454","77","125","252"
-        "111276","579","130","111","338"
-        "111241","973","236","237","500"
-        
-        The corresponding JSON data would be similar to the following (tabs and
-        other whitespace have been added for clarity).  Note the curly braces,
-        square brackets, and double-quotes!  These indicate which values should
-        be encoded as strings, and which values should be encoded as integers!
-        
-        {
-            "colHeaders":["ID","Total","Assignment 1","Assignment 2","Exam 1"],
-            "rowHeaders":["111278","111352","111373","111305","111399","111160",
-            "111276","111241"],
-            "data":[[611,146,128,337],
-                    [867,227,228,412],
-                    [461,96,90,275],
-                    [835,220,217,398],
-                    [898,226,229,443],
-                    [454,77,125,252],
-                    [579,130,111,338],
-                    [973,236,237,500]
-            ]
-        }
-    
-        Your task for this program is to complete the two conversion methods in
-        this class, "csvToJson()" and "jsonToCsv()", so that the CSV data shown
-        above can be converted to JSON format, and vice-versa.  Both methods
-        should return the converted data as strings, but the strings do not need
-        to include the newlines and whitespace shown in the examples; again,
-        this whitespace has been added only for clarity.
-    
-        NOTE: YOU SHOULD NOT WRITE ANY CODE WHICH MANUALLY COMPOSES THE OUTPUT
-        STRINGS!!!  Leave ALL string conversion to the two data conversion
-        libraries we have discussed, OpenCSV and json-simple.  See the "Data
-        Exchange" lecture notes for more details, including example code.
-    
-    */
-    
+     JSONArray records = new JSONArray(); // Container for all records
+     LinkedHashMap<String, String> jsonObject = new LinkedHashMap<>();
+     String[] record;
+     String jsonString = "";
+     
     @SuppressWarnings("unchecked")
     public static String csvToJson(String csvString) {
         
-        String results = "";
+        String results;
         
         try {
             
@@ -67,7 +24,41 @@ public class Converter {
             List<String[]> full = reader.readAll();
             Iterator<String[]> iterator = full.iterator();
             
-            // INSERT YOUR CODE HERE
+            JSONObject jsonObject = new JSONObject(); // Map
+            
+            JSONArray rowHeaders = new JSONArray();   // List
+            JSONArray mainData = new JSONArray();     // List
+            JSONArray colHeaders = new JSONArray();   // List
+            
+            for (String string : iterator.next()) {
+                
+                colHeaders.add(string);
+                
+            }
+            
+            while (iterator.hasNext()) {
+                
+                JSONArray rowData = new JSONArray(); 
+                
+                String[] rows = iterator.next();
+                rowHeaders.add(rows[0]);
+                
+                
+                for (int j = 1; j < rows.length; ++j) {
+                    
+                   rowData.add(Integer.parseInt(rows[j]));
+                    
+                }
+                
+                mainData.add(rowData);
+                
+            }
+           
+             jsonObject.put("colHeaders", colHeaders);
+             jsonObject.put("rowHeaders", rowHeaders);
+            jsonObject.put("data", mainData);
+            
+            results = JSONValue.toJSONString(jsonObject);
             
         }        
         catch(Exception e) { return e.toString(); }
@@ -78,14 +69,51 @@ public class Converter {
     
     public static String jsonToCsv(String jsonString) {
         
-        String results = "";
+        String results;
         
         try {
 
             StringWriter writer = new StringWriter();
             CSVWriter csvWriter = new CSVWriter(writer, ',', '"', '\n');
             
-            // INSERT YOUR CODE HERE
+           JSONParser parser = new JSONParser();
+           JSONObject jsonObject = (JSONObject)parser.parse(jsonString);
+           
+            JSONArray colHeaders = (JSONArray)jsonObject.get("colHeaders");
+            JSONArray rowHeaders = (JSONArray)jsonObject.get("rowHeaders");
+            JSONArray data = (JSONArray)jsonObject.get("data");
+            
+            String[] colStringArray = new String[colHeaders.size()];
+            String[] rowStringArray = new String[rowHeaders.size()];
+            String[] dataStringArray = new String[data.size()];
+            
+             for (int i = 0; i < colHeaders.size(); i++){
+                colStringArray[i] = colHeaders.get(i).toString();
+            }
+              
+            csvWriter.writeNext(colStringArray);
+            
+             for (int i = 0; i < rowHeaders.size(); i++){
+   
+                rowStringArray[i] = rowHeaders.get(i).toString();
+                dataStringArray[i] = data.get(i).toString();
+                
+            }
+             
+             for (int i = 0; i < dataStringArray.length; i++) {
+            
+                JSONArray dataValues = (JSONArray)parser.parse(dataStringArray[i]);
+                String[] row = new String[dataValues.size() + 1];
+                row[0] = rowStringArray[i];
+                
+                for(int j = 0; j < dataValues.size(); j++) {
+                    row[j+1] = dataValues.get(j).toString();
+                }
+                
+                csvWriter.writeNext(row);
+                
+            }
+             results = writer.toString();
             
         }
         
